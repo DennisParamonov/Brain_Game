8e232fdb7cebf4b533da59ae777715054c03a0ad
ichigowa <martj657@newschool.edu>
2015-03-19 15:44:10 -0400

    no message

---
+++ Midterm/Assets/Brain Dude/Animations.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+	public GameObject Lhand;
+	public GameObject Rhand;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Rhand.GetComponent<Animation>().Play("R Hand Iddle");
+		//Lhand.GetComponent<Animation>().Play("L Hand Iddle");
+		//Lhand.animation.Play ("L Hand Iddle");
+		//Rhand.imation.Play ("R Hand Iddle");
+
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Debug.Log ("Happy");
+			GetComponent<Animation> ().Play ("Happy");
+		} 
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Debug.Log ("Sad");
+			GetComponent<Animation> ().Play ("Sad");
+		} 
+		
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+			Debug.Log("Angry");
+			GetComponent<Animation>().Play("Angry");
+		}
+		
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			Debug.Log("Confused");
+			GetComponent<Animation>().Play("Confused");
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Debug.Log("Insane");
+			GetComponent<Animation>().Play("Insane");
+		}
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log("Wildcard");
+			GetComponent<Animation>().Play("Wildcard");
+		}
+		
+	}
+
+}
---
+++ Midterm/Assets/Brain Dude/test2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Debug.Log ("Sad");
+			GetComponent<Animation> ().Play ("L Hand Poke");
+		} else {
+			GetComponent<Animation>().Play("L Hand Iddle");
+		}
+	}
+}
